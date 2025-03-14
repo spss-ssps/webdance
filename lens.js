671da7bfdf7e298ba48bd9ad272ee81aeeac76ce
Sofia Pace <smp793@nyu.edu>
2025-03-13 22:47:42 -0400

    //changes black and white image

--- lens.js
+++ lens.js
@@ -10,34 +10,46 @@ let cropSize;
 let cropX; 
 let cropY;
 
-tempbutton.addEventListener('click', (event) => { takeshot()});
+tempbutton.addEventListener('click', (event) => { takeshot(event)});
 
 function takeshot() {
     let rect = lens.getBoundingClientRect();
     
-    let cursorX = event.clientX -rect.left;
-    let cursorY = event.clientY -rect.top;
+    let cursorX = event.clientX - rect.left;
+    let cursorY = event.clientY - rect.top;
  
-    let imgX = (cursorX / lens.width) * 200
-    let imgY = (cursorY / lens.height) * 100
+    let imgX = (cursorX / lens.width) * 200;
+    let imgY = (cursorY / lens.height) * 100;
 
     let cropSize = 100;
     let cropX = Math.max(0, imgX - cropSize / 2);
     let cropY = Math.max(0, imgY - cropSize / 2);
 
+    let drawX = (lens.width - cropSize) / 2;
+    let drawY = (lens.height - cropSize) / 2;
 
-    // let width= 200;
-    // let height= 100;
+    let context = lens.getContext('2d');
+    context.drawImage(video, cropX, cropY, cropSize, cropSize, drawX, drawY, cropSize, cropSize);
 
-    // console.log(width, height);
-    console.log(cropSize, cropX, cropY);
+    let imageData = context.getImageData(0, 0, lens.width, lens.height);
+    let data = imageData.data;
 
-    lens.getContext('2d').drawImage(video, cropX, cropY, cropSize, cropSize, 0, 0, lens.width, lens.height);
+    for (let i = 0; i < data.length; i += 4) {
+        let avg = (data[i] + data[i + 1] + data[i + 2]) / 2;
+        data[i] = avg; // red
+        data[i + 1] = avg; // green
+        data[i + 2] = avg; // blue
+    }
 
-    let data = lens.toDataURL('image/png');
-    output.style.background = `url('${data}') center/cover no-repeat`;
+    context.putImageData(imageData, 0, 0);
+
+    let dataURL = lens.toDataURL('image/png');
+    output.style.background = `url('${dataURL}') center/cover no-repeat`;
     output.style.transform = "scale(3)";
 }
+
+
+
 // function scaleLens() {
 //     let scale = 5;
 //     lens.width = video.videoWidth / scale;
